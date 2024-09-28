1b2529042fa90e1980820d26ab1636c088feeae2
Chad <chadtchiu@gmail.com>
2024-09-28 15:18:16 -0400

    de

--- README.md
+++
@@ -1,4 +0,0 @@
-# discofy
-We extension that makes your browser disco themed
-
-https://www.youtube.com/watch?v=Zt_6UXvoKHM
--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('discoStart').addEventListener('click', function() {
-    chrome.runtime.sendMessage({action: "startDisco"});
-});
-
-document.getElementById('discoStop').addEventListener('click', function() {
-    chrome.runtime.sendMessage({action: "stopDisco"});
-});
