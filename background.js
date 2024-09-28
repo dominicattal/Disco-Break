3bc6db0bf2585a3b24c0428226164b19c0fb472b
Alyson.m16 <alm57@njit.edu>
2024-09-28 15:14:00 -0400

    added dancefloor

--- background.js
+++ background.js
@@ -2,11 +2,10 @@ let discoMusic;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "startDisco") {
-        discoMusic = new Audio('assets/disco.mp3');
+        var discoMusic = new Audio('assets/disco.mp3');
         discoMusic.play();
     } else if (request.action === "stopDisco") {
         if (discoMusic) {
             discoMusic.pause();
         }
-    }
-});
+    }})
